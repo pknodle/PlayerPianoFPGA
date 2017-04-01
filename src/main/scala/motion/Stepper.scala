26f314ec1c4d41ffd475f986ff5470288f59fa9a
Philip Knodle <pknodle@gmail.com>
2017-04-01 15:40:39 -0400

    Added a fixed stepper motor drive controller
    It only does one velocity and goes as fast as it can...

---
+++ src/main/scala/motion/Stepper.scala
@@ -0,0 +1,38 @@
+// See LICENSE for license details.
+
+package motion
+
+import chisel3._
+
+class Stepper extends Module {
+  val io = IO(new Bundle {
+    val enable = Input(Bool());
+    val motorPhases  = Output(UInt(4.W))
+  })
+
+  val stepState  = Reg(UInt())
+
+
+  when(reset){
+    stepState := 0.U;
+    io.motorPhases := 0.U;
+  }.elsewhen(io.enable){
+    when(stepState === 0.U){
+      stepState := 1.U;
+      io.motorPhases := 12.U
+    }.elsewhen(stepState === 1.U){
+      stepState := 2.U;
+      io.motorPhases := 6.U;
+    }.elsewhen(stepState === 2.U){
+      stepState := 3.U;
+      io.motorPhases := 3.U;
+    }.elsewhen(stepState === 3.U){
+      stepState := 0.U;
+      io.motorPhases := 9.U;
+    }.otherwise {
+      stepState := 0.U;
+      io.motorPhases := 0.U;
+    }
+  }
+}
+
